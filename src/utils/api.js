d7a426c6ebcd56290bd7874d4cae90a6838f1956
Johny Thomas <johnythomas007@gmail.com>
2018-06-05 09:16:48 +0530

    Feat: Add API to get user details

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,3 +6,6 @@ export const getTrendingRepos = () =>
       "https://api.github.com/search/repositories?q=stars:%3E1+language:All&sort=stars&order=desc&type=Repositories"
     )
     .then(response => response.data.items);
+
+export const getUser = (id) => 
+    axios.get(`https://api.github.com/users/${id}`)
