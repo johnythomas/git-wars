5d0a6edc7137cb1746fc2a8c7042100380a80833
Johny Thomas <johnythomas007@gmail.com>
2018-06-06 10:09:13 +0530

    Feat: Store input in WarForm component in state

--- src/components/forms/WarForm.js
+++ src/components/forms/WarForm.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Form, Button } from "semantic-ui-react";
 
-const WarForm = () => (
-  <Form>
-    <Form.Field>
-      <label htmlFor="username">Username</label>
-      <input name="username" placeholder="Enter Username" />
-    </Form.Field>
-    <Button type="submit">Submit</Button>
-  </Form>
-);
+class WarForm extends Component {
+  state = {
+    username: ""
+  };
+
+  handleOnChange = e => {
+    const username = e.target.value;
+    this.setState(() => ({
+      username
+    }));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.username);
+  };
+
+  render() {
+    return (
+      <Form>
+        <Form.Field>
+          <label htmlFor="username">Username</label>
+          <input
+            name="username"
+            placeholder="Enter Username"
+            value={this.state.username}
+            onChange={this.handleOnChange}
+          />
+        </Form.Field>
+        <Button type="submit" onClick={this.handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    );
+  }
+}
+
+WarForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+};
 
 export default WarForm;
--- src/components/forms/WarFormGroup.js
+++ src/components/forms/WarFormGroup.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { Grid } from "semantic-ui-react";
 import WarForm from "./WarForm";
 
-const WarFormGroup = () => (
-  <Grid columns={2} divided>
-    <Grid.Row>
-      <Grid.Column>
-        <WarForm />
-      </Grid.Column>
-      <Grid.Column>
-        <WarForm />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-);
+class WarFormGroup extends Component {
+  state = {
+    user1: {},
+    user2: {}
+  };
+
+  onSubmit = user => username => {
+    console.log(`${user} value ${username}`);
+  };
+
+  render() {
+    return (
+      <Grid columns={2} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <WarForm onSubmit={this.onSubmit("user1")} />
+          </Grid.Column>
+          <Grid.Column>
+            <WarForm onSubmit={this.onSubmit("user2")} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
 
 export default WarFormGroup;
